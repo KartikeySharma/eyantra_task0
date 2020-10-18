59758b4b9d0d4bd8d27ca905b50a04bba78ce1ce
KartikeySharma <kartikey.bluebells@gmail.com>
2020-10-18 13:42:13 +0530

    Initial Commit'

--- scripts/node_turtle_revolve.py
+++ scripts/node_turtle_revolve.py
@@ -4,15 +4,13 @@
 import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
-import sys
 
 PI = 3.1415926535897
 
 # Function to print the current coordinates of the turtle 
 # and the angle it makes with the positive x axis
 def pose_callback(pose):
-	rospy.loginfo("Turtle X = %f: Y = %f: Theta = %f \n", pose.x, 
-					pose.y , pose.theta)
+	rospy.loginfo("Moving in circle \n Turtle X = %f: Y = %f: \n", pose.x, pose.y )
 
 # Main node function
 def main():
@@ -22,47 +20,49 @@ def main():
     
     # Initialising Publisher and Subscriber topics
     velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
-    vel_msg = Twist()
-    rospy.Subscriber('/turtle1/pose', Pose , pose_callback)    
+    v = Twist()
     
     # Meme est Potentia
     print( "Whomst have summoned the elevated one? ")
     
-    #speed=36 degrees/sec
-    r=10
-    angle=360
+    speed =0.8
+    angle =360
+    offset=0.25
+    # (speed*2*PI)/360 radians/sec
+    omega=0.7
     
-    omega=0.5 #speed*2*PI/360 radians/sec
     # Converting angle in degrees to radians
     radian_angle= (angle *2 * PI) / 360
     
     # Initialising initial positions 
-    vel_msg.linear.x=1
-    vel_msg.linear.y=0
-    vel_msg.linear.z=0
+    v.linear.x=speed
+    v.linear.y=0
+    v.linear.z=0
     
-    vel_msg.angular.x=0
-    vel_msg.angular.y=0
+    v.angular.x=0
+    v.angular.y=0
     
     # The angular velocity of z axis will be Omega
-    vel_msg.angular.z=abs(omega)
+    v.angular.z=abs(omega)
     
     # Finding the initial time
     t0=rospy.Time.now().to_sec()
     
     current_angle=0
-    
+    rospy.Subscriber('/turtle1/pose', Pose , pose_callback)
+        
     # Loops till the turtle completes one circle
-    while(current_angle < radian_angle):
-        velocity_publisher.publish(vel_msg)
+    while(current_angle <= radian_angle + offset):
+        velocity_publisher.publish(v)
         t1 = rospy.Time.now().to_sec()
         current_angle = omega*(t1-t0)
-    
+        
     # In the end, the turtle will be aligned horizontally    
-    vel_msg.angular.z = 0
+    v.angular.z = 0
     # Finally, the linear speed will be 0 as the turtle will stop
-    vel_msg.linear.x=0
-    velocity_publisher.publish(vel_msg)
+    v.linear.x=0
+    
+    velocity_publisher.publish(v)
     
 
 if __name__ == '__main__':
